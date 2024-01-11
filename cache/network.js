3bee0344c40d2cb821907b13e5e153e1931cc5c3
lautarohelp <lautarotoledo391@gmail.com>
2024-01-11 12:43:04 -0600

    Caché como un microservicio. Redis

--- cache/network.js
+++ cache/network.js
@@ -7,9 +7,7 @@ const router = express.Router();
 
 router.get('/:table', list);
 router.get('/:table/:id', get);
-router.post('/:table', insert);
 router.put('/:table', upsert);
-router.post('/:table/query', query);
 
 async function list(req, res, next) {
     const datos = await Store.list(req.params.table)
@@ -21,19 +19,9 @@ async function get(req, res, next) {
     response.success(req, res, datos, 200);
 }
 
-async function insert(req, res, next) {
-    const datos = await Store.insert(req.params.table, req.body)
-    response.success(req, res, datos, 200);
-}
-
 async function upsert(req, res, next) {
     const datos = await Store.upsert(req.params.table, req.body)
     response.success(req, res, datos, 200);
-}
-
-async function query(req, res, next) {
-    const datos = await store.query(req.params.table, req.body.query, req.body.join)
-    response.success(req, res, data, 200);
 }
 
 module.exports = router;
